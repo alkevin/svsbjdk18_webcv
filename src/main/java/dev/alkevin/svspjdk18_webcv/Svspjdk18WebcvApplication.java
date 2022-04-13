54f5e2b7428e7f9a53d898f580dd0d197b16fd02
Alves Kévin <kevin.alvespinto@gmail.com>
2022-04-13 12:11:35 +0200

    init spring boot server

---
+++ src/main/java/dev/alkevin/svspjdk18_webcv/Svspjdk18WebcvApplication.java
@@ -0,0 +1,13 @@
+package dev.alkevin.svspjdk18_webcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Svspjdk18WebcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Svspjdk18WebcvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alkevin/svspjdk18_webcv/Svspjdk18WebcvApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alkevin.svspjdk18_webcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Svspjdk18WebcvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
